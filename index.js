45df457b294a4d88311bcd0abe441a8233442c63
Wanderley Cabral da Silva <wcabraldasilvac@gmail.com>
2023-12-21 15:20:59 -0300

    :rocket: feat: refactor

--- index.js
+++ index.js
@@ -1,54 +1,43 @@
+const CELSIUS_FAHRENHEIT = (celsius) => (celsius * 9) / 5 + 32
+const CELSIUS_KELVIN = (celsius) => celsius + 273.15
+
+const FAHRENHEIT_CELSIUS = (fahrenheit) => (fahrenheit - 32) * (5 / 9)
+const FAHRENHEIT_KELVIN = (fahrenheit) => ((fahrenheit - 32) * 5) / 9 + 273.15
+
+const KELVIN_CELSIUS = (kelvin) => kelvin - 273.15
+const KELVIN_FAHRENHEIT = (kelvin) => ((kelvin - 273.15) * 9) / 5 + 32
+
 function convertTemperature() {
-    let val = document.getElementById('input_value')
+    let val = parseFloat(document.getElementById('input_value').value)
     let result = document.getElementById('result')
-    let output = document.getElementById('output')
-    let input = document.getElementById('input')
+    let input = document.getElementById('input').value
+    let output = document.getElementById('output').value
 
-    val.addEventListener('keyup', convertTemperature)
-    input.addEventListener('change', convertTemperature)
-    output.addEventListener('change', convertTemperature)
+    let convertedResult
 
-    let inputValue = input.value
-    let outputValue = output.value
-
-    /* FORMULAS DE CONVERSÃO 
-    celsius_fahrenheit = (celsius * 9/5) + 32
-    celsius_kelvin = celsius + 273.15
-
-    fahrenheit_celsius = (fahrenheit - 32) * 5/9
-    fahrenheit_kelvin = (fahrenheit - 32) * 5/9 + 273.15
-
-    kelvin_celsius = kelvin - 273.15
-    kelvin_fahrenheit = (kelvin - 273.15) * 9/5 + 32
-    */
-
-    // conversão para celsius
-    if (inputValue === 'celsius' && outputValue === 'fahrenheit') {
-        result.innerHTML = Number((val.value * 9) / 5) + 32
-    } else if (inputValue === 'celsius' && outputValue === 'kelvin') {
-        result.innerHTML = Number(val.value + 273.15)
-    } else if (inputValue === 'celsius' && outputValue === 'celsius') {
-        result.innerHTML = val.value
-    }
-    // conversão para fahrenheit
-    if (inputValue === 'fahrenheit' && outputValue === 'celsius') {
-        result.innerHTML = Number((val.value - 32) * 9) + 5
-    } else if (inputValue === 'fahrenheit' && outputValue === 'kelvin') {
-        result.innerHTML = Number(((val.value - 32) * 5) / 9) + 273.15
-    } else if (inputValue === 'fahrenheit' && outputValue === 'fahrenheit') {
-        result.innerHTML = val.value
+    if (input === 'celsius' && output === 'fahrenheit') {
+        convertedResult = CELSIUS_FAHRENHEIT(val)
+    } else if (input === 'celsius' && output === 'kelvin') {
+        convertedResult = CELSIUS_KELVIN(val)
+    } else if (input === 'fahrenheit' && output === 'celsius') {
+        convertedResult = FAHRENHEIT_CELSIUS(val)
+    } else if (input === 'fahrenheit' && output === 'kelvin') {
+        convertedResult = FAHRENHEIT_KELVIN(val)
+    } else if (input === 'kelvin' && output === 'celsius') {
+        convertedResult = KELVIN_CELSIUS(val)
+    } else if (input === 'kelvin' && output === 'fahrenheit') {
+        convertedResult = KELVIN_FAHRENHEIT(val)
     }
 
-    //  conversão para kelvin
-    if (inputValue === 'kelvin' && outputValue === 'celsius') {
-        result.innerHTML = Number(val.value - 273.15)
-    } else if (inputValue === 'kelvin' && outputValue === 'fahrenheit') {
-        result.innerHTML = Number(((val.value - 273.15) * 9) / 5) + 32
-    } else if (inputValue === 'kelvin' && outputValue === 'kelvin') {
-        result.innerHTML = val.value
-    }
+    result.innerHTML = convertedResult.toFixed(2)
 }
 
+// Adicionar um único ouvinte de evento para os elementos relevantes
+document.getElementById('input_value').addEventListener('input', convertTemperature)
+document.getElementById('input').addEventListener('change', convertTemperature)
+document.getElementById('output').addEventListener('change', convertTemperature)
+
 function reset() {
+    document.getElementById('input_value').value = ''
     convertTemperature()
 }
